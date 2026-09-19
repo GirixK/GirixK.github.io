9c3bfdb80311fe665ed70f12b6abe47a6dedb04d
GirixK <GirixK@users.noreply.github.com>
2026-09-19 20:41:01 +0200

    i'm tired boss

---
+++ javascript/copytext.js
@@ -0,0 +1,8 @@
+document.querySelectorAll(".copy").forEach(copyButton => {
+    copyButton.addEventListener("click", () => {
+        const targetElement = document.querySelector(copyButton.dataset.copy);
+        const textToCopy = targetElement.textContent;
+
+        console.log(textToCopy);
+    });
+});
